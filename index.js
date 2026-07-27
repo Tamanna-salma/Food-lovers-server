ca47fbc64a55aa109c90dd0f53824873cbbda6ad
Tamanna-salma <tamannaummey26@gmail.com>
2026-07-28 05:50:50 +0600

    fix: update cors origin for production frontend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ["http://localhost:5173", "https://food-network-lover.netlify.app"],
+  origin: ["http://localhost:5173", "https://jovial-belekoy-f54712.netlify.app"],
   credentials: true
 }));
 app.use(express.json());
